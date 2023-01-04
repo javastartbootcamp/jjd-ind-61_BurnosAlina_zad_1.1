e7015c47ab02f0ca960f12d75f78336fd36319fe
Alina Burnos <burnosalina@gmail.com>
2023-01-04 12:03:09 +0100

    Dodaje tekst do pliku Zadanie

--- src/main/java/Zadanie.java
+++ src/main/java/Zadanie.java
@@ -1,5 +1,5 @@
 public class Zadanie {
     public static void main(String[] args) {
-        System.out.println("Uczę\nsię\nprogramowania");
+        System.out.println("Uczę\nsię\nprogramowania\ni\ncommitowania");
     }
 }
